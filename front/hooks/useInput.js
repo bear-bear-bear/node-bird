58f9124c45cbf2416bc9b61393d49b3e075e0c52
bearbearbear <bear-bear-bear@kakao.com>
2021-04-09 05:31:29 +0900

    fix: useInput 훅스를 익명 화살표 함수에서 함수 표기법으로 변경

--- front/hooks/useInput.js
+++ front/hooks/useInput.js
@@ -1,10 +1,10 @@
 import { useState, useCallback } from 'react';
 
-export default (initialValue = null, deps = []) => {
+export default function useInput(initialValue = null, deps = []) {
   const [value, setValue] = useState(initialValue);
   const handler = useCallback((e) => {
     setValue(e.target.value);
   }, deps);
 
   return [value, handler];
-};
+}
