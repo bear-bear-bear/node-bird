332e8bc92c1c106a6e759430599ad4978385ff33
bearbearbear <bear-bear-bear@kakao.com>
2021-04-09 00:40:22 +0900

    feat: 로그인 시 프로필 부분을 담당하는 컴포넌트 초기 설정

--- front/components/UserProfile/index.js
+++ front/components/UserProfile/index.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { Card, Avatar, Button } from 'antd';
 
-const UserProfile = () => {
-  return <div>UserProfile</div>;
+const UserProfile = ({ setIsLoggedIn }) => {
+  const onLogOut = useCallback(() => {
+    setIsLoggedIn(false);
+  }, [])
+
+  return (
+    <Card
+      actions={[
+        <div key="twit">트윗<br />@</div>,
+        <div key="following">팔로잉<br />@</div>,
+        <div key="follower">팔로워<br />@</div>
+      ]}
+    >
+      <Card.Meta 
+        avatar={<Avatar>BEAR</Avatar>}
+        title="bear"
+      />
+      <Button onClick={onLogOut}>로그아웃</Button>
+    </Card>
+  );
 };
 
 export default UserProfile;
