5b9fccca846af39edf04ba24be1b0a55992e209f
bearbearbear <bear-bear-bear@kakao.com>
2021-04-28 23:43:23 +0900

    feat: 팔로우 언팔로우 구현

--- front/components/PostCard/index.js
+++ front/components/PostCard/index.js
@@ -13,6 +13,7 @@ import {
 import PostImages from '../PostImages';
 import CommentForm from '../CommentForm';
 import PostCardContent from '../PostCardContent';
+import FollowButton from '../FollowButton';
 import { REMOVE_POST_REQUEST } from '../../reducers/post';
 
 import * as S from './styles';
@@ -71,6 +72,7 @@ const PostCard = ({ post }) => {
             <EllipsisOutlined />
           </Popover>,
         ]}
+        extra={<FollowButton post={post} />}
       >
         <Card.Meta
           avatar={<Avatar>{post.User.nickname[0]}</Avatar>}
