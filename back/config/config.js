a6c701d3e4589228c133493f1ff6aa29443aae7b
bearbearbear <bear-bear-bear@kakao.com>
2021-05-18 01:32:36 +0900

    feat: sequelize config test, production에 password 부여

--- back/config/config.js
+++ back/config/config.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 
-const password = process.env.MYSQL_PASSWORD;
+const password = process.env.DB_PASSWORD;
 
 module.exports = {
   'development': {
@@ -12,14 +12,14 @@ module.exports = {
   },
   'test': {
     'username': 'root',
-    'password': null,
+    'password': password,
     'database': 'database_test',
     'host': '127.0.0.1',
     'dialect': 'mysql'
   },
   'production': {
     'username': 'root',
-    'password': null,
+    'password': password,
     'database': 'database_production',
     'host': '127.0.0.1',
     'dialect': 'mysql'
