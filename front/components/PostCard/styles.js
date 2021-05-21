cf106bd6af19a1698f88d1f72d9856e933fb8cf6
bearbearbear <bear-bear-bear@kakao.com>
2021-05-22 04:46:24 +0900

    feat: 옅은 그림자선으로 게시글 구분

--- front/components/PostCard/styles.js
+++ front/components/PostCard/styles.js
@@ -1,5 +1,13 @@
 import styled from 'styled-components';
 
 export const CardWrapper = styled.div`
+  box-shadow: 0 0 1px #fcfcfc,
+                    0 0 2px #fcfcfc,
+                    0 0 4px #fcfcfc,
+                    0 0 10px #fcfcfc;
   margin-bottom: 1.33rem;
+
+  &:first-child {
+    margin-top: 0.33rem;
+  }
 `;
