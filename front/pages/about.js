74db85c98fa1b0be3dc64b088345a2de15087553
bearbearbear <bear-bear-bear@kakao.com>
2021-06-12 20:09:10 +0900

    feat: 내 정보를 나타내는 about 페이지 뼈대 작성

---
+++ front/pages/about.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import Head from 'next/head';
+// import { END } from 'redux-saga';
+
+import { Avatar, Card } from 'antd';
+import AppLayout from '../components/AppLayout';
+// import wrapper from '../store/configureStore';
+// import { LOAD_USER_REQUEST } from '../reducers/user';
+
+const About = () => {
+  const { userInfo } = useSelector((state) => state.user);
+
+  return (
+    <AppLayout>
+      <Head>
+        <title>bear-bear-bear | NodeBird</title>
+      </Head>
+      {userInfo
+        ? (
+          <Card
+            actions={[
+              <div key="twit">
+                트윗하기
+                <br />
+                {userInfo.Posts}
+              </div>,
+              <div key="following">
+                팔로잉
+                <br />
+                {userInfo.Followings}
+              </div>,
+              <div key="follower">
+                팔로잉
+                <br />
+                {userInfo.Followers}
+              </div>,
+            ]}
+          >
+            <Card.Meta
+              avatar={<Avatar>{userInfo.nickname[0]}</Avatar>}
+              title={userInfo.nickname}
+              description="개발자 지망생"
+            />
+          </Card>
+        )
+        : null}
+    </AppLayout>
+  );
+};
+
+export default About;
