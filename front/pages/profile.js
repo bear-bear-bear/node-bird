348402447442f968c645da597b0fd19c8114a67a
bearbearbear <bear-bear-bear@kakao.com>
2021-07-18 01:30:35 +0900

    feat: 프로필 페이지와 회원가입 페이지에 SSR로 유저 정보 받아오기 추가

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -2,11 +2,14 @@ import React, { useEffect } from 'react';
 import Head from 'next/head';
 import Router from 'next/router';
 import { useSelector, useDispatch } from 'react-redux';
+import { END } from 'redux-saga';
+import axios from 'axios';
 
 import AppLayout from '../components/AppLayout';
 import NicknameEditForm from '../components/NicknameEditForm';
 import FollowList from '../components/FollowList';
-import { LOAD_FOLLOWERS_REQUEST, LOAD_FOLLOWINGS_REQUEST } from '../reducers/user';
+import { LOAD_FOLLOWERS_REQUEST, LOAD_FOLLOWINGS_REQUEST, LOAD_MY_INFO_REQUEST } from '../reducers/user';
+import wrapper from '../store/configureStore';
 
 const Profile = () => {
   const dispatch = useDispatch();
@@ -38,5 +41,22 @@ const Profile = () => {
     </>
   );
 };
+
+export const getServerSideProps = wrapper.getServerSideProps(async (context) => {
+  const { req, store } = context;
+
+  const cookie = req ? req.headers.cookie : '';
+  axios.defaults.headers.Cookie = ''; // 이전 CDN에 올라와 있던 쿠키 초기화
+  if (req && cookie) {
+    axios.defaults.headers.Cookie = cookie;
+  }
+
+  store.dispatch({
+    type: LOAD_MY_INFO_REQUEST,
+  });
+
+  store.dispatch(END);
+  await store.sagaTask.toPromise();
+});
 
 export default Profile;
--- front/pages/signup.js
+++ front/pages/signup.js
@@ -1,10 +1,14 @@
 import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import { END } from 'redux-saga';
+import axios from 'axios';
 import Head from 'next/head';
 import Router from 'next/router';
 
 import AppLayout from '../components/AppLayout';
 import SignupForm from '../components/SignupForm';
+import wrapper from '../store/configureStore';
+import { LOAD_MY_INFO_REQUEST } from '../reducers/user';
 
 const Signup = () => {
   const { me } = useSelector((state) => state.user);
@@ -30,5 +34,22 @@ const Signup = () => {
     </AppLayout>
   );
 };
+
+export const getServerSideProps = wrapper.getServerSideProps(async (context) => {
+  const { req, store } = context;
+
+  const cookie = req ? req.headers.cookie : '';
+  axios.defaults.headers.Cookie = ''; // 이전 CDN에 올라와 있던 쿠키 초기화
+  if (req && cookie) {
+    axios.defaults.headers.Cookie = cookie;
+  }
+
+  store.dispatch({
+    type: LOAD_MY_INFO_REQUEST,
+  });
+
+  store.dispatch(END);
+  await store.sagaTask.toPromise();
+});
 
 export default Signup;
