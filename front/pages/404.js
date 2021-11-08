db4acded21bbbfefca4e2623f519730ab587e133
bearbearbear <ghlwkdthfl@gmail.com>
2021-11-08 21:41:41 +0900

    feat: 커스텀 404 페이지 추가

--- front/pages/404.js
+++ front/pages/404.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import AppLayout from '../components/AppLayout/index';
 
 export default function Custom404() {
   return (
-    <AppLayout>
+    <section style={{
+      width: '100%',
+      height: '100vh',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}
+    >
       <h1>404 Error - Page Not Found</h1>
-    </AppLayout>
+    </section>
   );
 }
