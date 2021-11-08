14edb7e434dac4622ca3573f9994d6c71e8a4cd5
bearbearbear <ghlwkdthfl@gmail.com>
2021-11-08 21:39:24 +0900

    feat: 사용하지 않는 about 페이지 삭제, 404 페이지 추가

---
+++ front/pages/404.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import AppLayout from '../components/AppLayout/index';
+
+export default function Custom404() {
+  return (
+    <AppLayout>
+      <h1>404 Error - Page Not Found</h1>
+    </AppLayout>
+  );
+}
