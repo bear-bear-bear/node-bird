6956540c9aedb03da5b032271fb7d04e78037ee1
bearbearbear <ghlwkdthfl@gmail.com>
2021-11-09 01:36:53 +0900

    feat: 프론트측 포트 설정

--- back/app.js
+++ back/app.js
@@ -34,7 +34,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(morgan('dev'));
 }
 app.use(cors({
-  origin: ['http://localhost:3000', 'nodebird.com'],
+  origin: ['http://localhost:80', 'nodebird.com'],
   credentials: true,
 }));
 app.use('/', express.static(path.join(__dirname, 'uploads')));
