3fdb5229e14c775ab6779b109c5497207ceee786
bearbearbear <ghlwkdthfl@gmail.com>
2021-11-10 14:04:30 +0900

    feat: 이미지 resizing lambda 추가

---
+++ lambda/index.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const AWS = require('aws-sdk');
+const sharp = require('sharp');
+
+const s3 = new AWS.S3();
+
+exports.handler = async (event, context, callback) => {
+  const Bucket = event.Records[0].s3.bucket.name; // 'bearsns'
+  const Key = decodeURIComponent(event.Records[0].s3.object.key); // original/example_abc.png
+  const basename = path.basename(Key);
+  const extname = path.extname(Key).toLowerCase();
+  const requiredFormat = extname === 'jpg' ? 'jpeg' : extname;
+
+  console.log({ Bucket, Key, basename, extname, requiredFormat });
+
+  try {
+    const s3Object = await s3.getObject({ Bucket, Key }).promise();
+    console.log('original', s3Object.Body.length);
+    const resizedImage = await sharp(s3Object.Body)
+                                  .resize(400, 400, { fit: 'inside' })
+                                  .toFormat(requiredFormat)
+                                  .toBuffer();
+    
+    await s3.putObject({
+      Bucket,
+      Key: `thumb/${filename}`,
+      Body: resizedImage,
+    }).promise();
+    console.log('put', resizedImage.length);
+    return callback(null, `thumb/${filename}`);
+  } catch (error) {
+    console.error(error);
+    return callback(error);
+  }
+}
