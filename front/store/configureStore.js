5c29e2dac73bee61e75ee84b91cfcf18e02b3e6f
bearbearbear <ghlwkdthfl@gmail.com>
2021-11-10 16:11:43 +0900

    feat: production에서도 devtools가 동작하도록 변경

--- front/store/configureStore.js
+++ front/store/configureStore.js
@@ -1,6 +1,6 @@
 /* eslint-disable indent */
 import { createWrapper } from 'next-redux-wrapper';
-import { applyMiddleware, compose, createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import createSagaMiddleware from 'redux-saga';
 
@@ -31,9 +31,7 @@ const loggerMiddleware = () => (next) => (action) => {
 const configureStore = () => {
   const sagaMiddleware = createSagaMiddleware();
   const middlewares = [sagaMiddleware, loggerMiddleware];
-  const enhancer = process.env.NODE_ENV === 'production'
-                      ? compose(applyMiddleware(...middlewares))
-                      : composeWithDevTools(applyMiddleware(...middlewares));
+  const enhancer = composeWithDevTools(applyMiddleware(...middlewares));
   const store = createStore(reducer, enhancer);
   store.sagaTask = sagaMiddleware.run(rootSaga);
   return store;
