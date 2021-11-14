7f722d71bda9132e917caf4a01ebd8562ec71874
bearbearbear <ghlwkdthfl@gmail.com>
2021-11-15 00:11:33 +0900

    feat: 에러 문구 수정

--- back/passport/local.js
+++ back/passport/local.js
@@ -15,7 +15,7 @@ module.exports = () => {
   
       // 이메일 유무 검증
       if (!user) {
-        return done(null, false, { reason: '존재하지 않는 사용자입니다!' });
+        return done(null, false, { reason: '존재하지 않는 사용자입니다.' });
       }
   
       // 패스워드 유효성 검증
