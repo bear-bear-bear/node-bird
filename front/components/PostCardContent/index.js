3263c898266504a9752aadddfca7de065ae1db2f
bearbearbear <ghlwkdthfl@gmail.com>
2021-11-14 21:53:35 +0900

    fix: 해쉬태그 링크 컬러 파란색으로 안 나오는 문제 수정, 해쉬태그는 prefetch false 부여

--- front/components/PostCardContent/index.js
+++ front/components/PostCardContent/index.js
@@ -2,21 +2,25 @@ import React from 'react';
 import Link from 'next/link';
 import PropTypes from 'prop-types';
 
-const PostCardContent = ({ postData }) => (
-  <div>
-    {postData.split(/(#[^\s#]+)/g).map((v, i) => {
-      if (v.match(/(#[^\s#]+)/)) {
-        return (
-          // eslint-disable-next-line react/no-array-index-key
-          <Link href={`/hashtag/${v.slice(1)}`} key={i}>
-            <a href={`hashtag/${v.slice(1)}`}>{v}</a>
-          </Link>
-        );
-      }
-      return v;
-    })}
-  </div>
-);
+const PostCardContent = ({ postData }) => {
+  const hashtagRegex = /(#[^\s#]+)/g;
+
+  return (
+    <div>
+      {postData.split(hashtagRegex).map((v, i) => {
+        if (v.match(hashtagRegex)) {
+          return (
+            // eslint-disable-next-line react/no-array-index-key
+            <Link href={`/hashtag/${v.slice(1)}`} key={i} prefetch={false}>
+              <a style={{ color: 'rgb(0, 0, 238)' }}>{v}</a>
+            </Link>
+          );
+        }
+        return v;
+      })}
+    </div>
+  );
+};
 
 PostCardContent.propTypes = {
   postData: PropTypes.string.isRequired,
